2cfc27b33cd0112c7e19da287340877ef7b15ded
OttoBisno <ottobisno444@gmail.com>
2023-01-18 17:08:00 -0800

    Created public folder and completed Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import '../styles/Header.css';
+import Navigation from './Navigation';
+
+export default function Header() {
+    return (
+        <div className='navbar-container containter-fluid'>
+            <Navigation />
+        </div>
+    );
+};
