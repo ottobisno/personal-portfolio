75bfdf3a49074f0f6d14a48d4d5c04ee0dbad791
OttoBisno <ottobisno444@gmail.com>
2023-02-08 19:51:33 -0800

    Added project 3

--- src/projectData.js
+++ src/projectData.js
@@ -1,6 +1,5 @@
 import dataDepositsImg from './images/data-deposits.jpg';
 import movieSearchImg from './images/movie-search-app.jpg';
-import noteTakerImg from './images/note-taker.jpg';
 import socialNetworkImg from './images/social-network-api.jpg';
 import techBlogImg from './images/tech-blog.jpg';
 import weatherDashImg from './images/weather-dashboard.jpg';
